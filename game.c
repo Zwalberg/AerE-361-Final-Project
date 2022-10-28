2a4e2520fce96385df11fd851e55edb7fb3ee791
Zach Walberg <zwalberg@iastate.edu>
2022-10-28 03:55:23 +0000

    Update game files

--- game.c
+++ game.c
@@ -4,10 +4,49 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include"numgen.c"
+#include<unistd.h>
 
 int main(){
-    int i,j,card;
+    int i=0,j,deal[4],player,dealer;
+    int cards[52][4];
+    int a,b,c,d;
+    char cards1[100];
+    FILE * fp;
+
+    fp = fopen("Card_List.csv","r");
+
+    if (fp == NULL){
+        fprintf(stderr,"Card list did not open correctly.\nEXITING PROGRAM\n");
+        exit(3);
+    }
+
+    do{
+        fscanf(fp,"%d,%d,%d,%d",&a,&b,&c,&d);
+        cards[i][0] = a;
+        cards[i][1] = b;
+        cards[i][2] = c;
+        cards[i][3] = d;
+        i++;
+    }while(fgets(cards1,100,fp) != NULL);
+
+
     printf("Welcome to Blackjack\n");
+    for (i = 1; i <= 4; i++){
+        int index = i-1;
+        deal[index] = card_draw(i);
+        printf("%d\n",deal[index]);
+        printf("\t%d %d\n",cards[deal[index]-1][1],cards[deal[index]-1][2]);
+    }
+    
+
+    player = cards[deal[0]-1][2]+cards[deal[2]-1][2];
+    dealer = cards[deal[1]-1][2]+cards[deal[3]-1][2];
+
+    printf("Player total: %d\n",player);
+    printf("Dealer total: %d\n",dealer);
+
+    
 
 
 
--- numgen.c
+++ numgen.c
@@ -6,58 +6,22 @@
 #include<string.h>
 #include<time.h>
 
-int card_draw(int num);
+int card_draw();
 
-int main(){
-
-    int i = 0,j,draw;
-    int cards[52][4];
-    int a,b,c,d;
-    char cards1[100],input[100];
-    FILE * fp;
-    FILE * fp1;
-
-    fp = fopen("Card_List.csv","r");
-
-    if (fp == NULL){
-        fprintf(stderr,"Card list did not open correctly.\nEXITING PROGRAM\n");
-        exit(3);
-    }
-
-    do{
-        fscanf(fp,"%d,%d,%d,%d",&a,&b,&c,&d);
-        cards[i][0] = a;
-        cards[i][1] = b;
-        cards[i][2] = c;
-        cards[i][3] = d;
-        /*for (j = 0; j < 4; j++){
-        fprintf(stdout,"%*d",3,cards[i][j]);
-        }
-        printf("\n");*/
-        i++;
-    }while(fgets(cards1,100,fp) != NULL);
-
-    printf("Input number of cards to draw: ");
-    scanf("%d",&draw);
-    card_draw(draw);
-    
- return 0;
-}
-
-int card_draw(int num){
-    int upper,lower;
+int card_draw(int count){
+    int upper,lower,draw,i;
+    time_t t;
 
     upper = 52;
     lower = 1;
 
-    srand(time(0));
+    srand(time(NULL));
 
-    printf("The random numbers are: ");
-    for (int i = 0; i < num; i++) {
-        int num = (rand() % (upper - lower + 1)) + lower;
-        fprintf(stdout,"%*d",3,num);
+    for(i = 0; i < count; i++){
+        draw = (rand() % (upper - lower + 1)) + lower;
+        //printf(" %d",draw);
     }
-    printf("\n");
+    //printf("\n");
 
-    return 0;
+    return draw;
 }
